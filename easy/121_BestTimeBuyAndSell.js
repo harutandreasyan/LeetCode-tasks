3ceadc0227199cd5917c8634f7cb993d58ffd1ad
harut <har8andreasyan@gmail.com>
2025-04-09 00:13:48 +0400

    new tasks

---
+++ easy/121_BestTimeBuyAndSell.js
@@ -0,0 +1,42 @@
+/*
+    You are given an array prices where prices[i] is the price
+    of a given stock on the ith day.
+
+    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+    Example 1:
+    Input: prices = [7,1,5,3,6,4]
+    Output: 5
+    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+    
+    Example 2:
+    Input: prices = [7,6,4,3,1]
+    Output: 0
+    Explanation: In this case, no transactions are done and the max profit = 0.
+*/
+
+var maxProfit = function (prices) {
+	let minPrice = prices[0]
+	let maxProf = 0
+
+	for (let i = 1; i < prices.length; ++i) {
+		if (prices[i] < minPrice) {
+			minPrice = prices[i]
+		}
+
+		let currentProf = prices[i] - minPrice
+
+		if (currentProf > maxProf) {
+			maxProf = currentProf
+		}
+	}
+
+	return maxProf
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])) // 5
+console.log(maxProfit([7, 6, 4, 3, 1])) // 0
+console.log(maxProfit([1, 2, 3, 4, 5])) // 4
